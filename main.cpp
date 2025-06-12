82256bdc04077970f9956e3801ef39ef3481c905
Brian Tum <brian.tum10@yahoo.com>
2025-06-12 22:16:38 +0300

    correcting optional string dereferencing

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@
 
 void explainError(std::string msg);
 constexpr std::optional<std::string> message(int argc, char* argv[]);
+constexpr std::optional<std::string> iconPath(std::string home);
 
 // 1. get home path
 constexpr std::optional<std::string> returnHome() {
@@ -47,13 +48,24 @@ constexpr std::optional<std::string> currentTask(std::string todoPath) {
 
 // 3. add todo elements
 constexpr bool addTask(std::string todoPath, std::string task) {
+	auto home {returnHome()};
+	if (!home) {
+		explainError("Home env not defined");
+		return false;
+	}
+	auto iconPth {iconPath(*home)};
+	if (!iconPth)  {
+		explainError("icon path not defined");
+		return false;
+	}
 	std::ofstream todoFile {todoPath, std::ios::app};
 	if (!todoFile) {
 		todoFile.close();
 		std::string error {todoPath + " does not seem to exist"};
 		explainError(error);
+		return false;
 	} else {
-		todoFile << task << "\n";
+		todoFile <<*iconPth<<"- " << task << "\n";
 		todoFile.close();
 		return true;
 	}
@@ -66,21 +78,31 @@ constexpr bool addTask(std::string todoPath, std::string task) {
 // 5. handle args
 void handleArgs(int argc, char* argv[]) {
 	auto home {returnHome()};
-	std::string todo {"/todo/todo.txt"};
-	std::string todoPath {*home + todo}; // deconstruct home in if statement 
-
-	std::string firstArg {argv[1]};
 	auto joinedArgs {message(argc, argv)};
-
-	if ("add" == firstArg) {
-		if (addTask(todoPath, *joinedArgs)) {
-			std::cout<<"Successfully added "<<*joinedArgs<<"\n";
-		} else {
-			explainError("adding todo item");
-		}
-	} else {
-		explainError("unknown arg");
+	if (!joinedArgs) {
+		explainError("error reading todo task");
+		return;
 	}
+	if (home) {
+		std::string todo {"/todo/todo.txt"};
+		std::string todoPath = *home + todo;
+
+		std::string firstArg {argv[1]};
+
+		if ("add" == firstArg) {
+			if (addTask(todoPath, *joinedArgs)) {
+				std::cout<<"Successfully added "<<*joinedArgs<<"\n";
+			} else {
+				explainError("adding todo item");
+			}
+		} else {
+			explainError("unknown arg");
+			return;
+		}
+	}	else {
+		explainError("Home env not defined");
+		return;
+	}	
 }
 // 6. add "alarm" when task are not done before 10pm
 // 7. Reminder to set next days TODO
@@ -88,39 +110,45 @@ void handleArgs(int argc, char* argv[]) {
 int main(int argc, char* argv[]) {
 	if (argc < 2) {
 		explainError("few args");
+		return 1;
 	}
 	
 	handleArgs(argc, argv);
 	return 0;
 }
-// helper functions 
+
+// 8. helper functions 
 
 void explainError(std::string msg) {
-	std::string error {msg};
-	std::cerr<<"Error caused by: "<<error<<"\n";
+	std::string error{msg};
+	std::cout<<"Error caused by: "<<error<<"\n";
 	std::cerr<<" program usage: \n"; 
 	std::cerr<<" ./todo add finish this project\n"; 
-	std::cerr<<" ./todo done //removes the active task"; 
-	return;
+	std::cerr<<" ./todo done //removes the active task\n"; 
 }
 
 // helper func to combine string after argv[1]
 constexpr std::optional<std::string> message(int argc, char* argv[]) {
-	if (argc < 3) {
-		std::cerr<<"Add arguments \n";
+	if (argc < 2) {
+		explainError("few args");
 		return std::nullopt;
-	}
+	} else {
 	
-	std::string joinedArgs;
-	for (int i{2}; i<argc;++i) {
-		joinedArgs += argv[i];	
-		joinedArgs += " ";
-	}
+		std::string joinedArgs;
+		for (int i{2}; i<argc;++i) {
+			joinedArgs += argv[i];	
+			if (i < argc -1) {
+				joinedArgs += " ";
+			}
+		}
 
-	return joinedArgs;
+		return joinedArgs;
+	}
 }
 
 // get icons path
 constexpr std::optional<std::string> iconPath(std::string home) {
-	
+	std::string ic {"/todo/icons/thunder.png"};
+	std::string icons {home + ic};
+	return icons;
 }
